4a77320256027c18627b48b3a27d6eb748e56192
Andrew Kornder <andrewkornder@gmail.com>
2023-04-27 03:02:08 -0400

    .

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,22 +1,15 @@
 -- help: (https://github.com/mfussenegger/nvim-jdtls#configuration-verbose)
 --
-local config = {
-    cmd = {'/Users/akornder25/.config/jdtls/bin/jdtls'},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-    autostart=true,
-}
-require('jdtls').start_or_attach(config)
---[[
 
-local data_dir, jar, config
+local data_dir, jar, config_dir
 if vim.fn.has('macunix') == 0 then
     data_dir = "C:/Users/usr/AppData/Local/nvim" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
     jar = "C:/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"
-    config = "C:/jdtls/config_win"
+    config_dir = "C:/jdtls/config_win"
 else
-    data_dir = "~/.config/nvim/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-    jar = '/Users/akornder25/.config/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar'
-    config = "~/.config/jdtls/config_mac/"
+    data_dir = "~/.config_dir/nvim/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+    jar = '/Users/akornder25/.config_dir/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar'
+    config_dir = "~/.config_dir/jdtls/config_mac/"
 end
 
 
@@ -33,7 +26,7 @@ local config = {
         "--add-opens", "java.base/java.util=ALL-UNNAMED",
         "--add-opens", "java.base/java.lang=ALL-UNNAMED",
         "-jar", jar,
-        "-configuration", config,
+        "-configuration", config_dir,
         "-data", data_dir,
     },
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
@@ -47,4 +40,4 @@ local config = {
     },
 }
 
-require("jdtls").start_or_attach(config)]]--
+require("jdtls").start_or_attach(config)
--- lua/usr/set.lua
+++ lua/usr/set.lua
@@ -18,7 +18,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "120"
 
 vim.opt.autochdir = false
 
